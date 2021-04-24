6130c20b58f0b9f79d841439075c811bae204c24
smSpark <you@gmail.com>
2021-04-24 21:25:48 +0300

    complete

---
+++ src/pages/CurrencyList/CurrencyList.tsx
@@ -0,0 +1,24 @@
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { CurrencyTable } from "../../components/CurrencyTable/CurrencyTable";
+import { FullStats } from "../../components/FullStats/FullStats";
+import { data } from "../../mocks/fullstat.json";
+import { fetchCurrencyData } from "../../store/currency/action";
+
+// FIXME: оптимизировать, будет много перерисовок с dispatch здесь
+const CurrencyList: React.FC = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCurrencyData());
+  }, [dispatch]);
+
+  return (
+    <>
+      <FullStats data={data} />
+      <CurrencyTable />
+    </>
+  );
+};
+
+export { CurrencyList };
